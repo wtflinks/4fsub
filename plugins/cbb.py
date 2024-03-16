923b9b499ab0d889c4bfd9162e53b9d8b4765fb0
animeotaku1091 <154030645+animeotaku1091@users.noreply.github.com>
2024-03-16 20:24:20 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,6 @@
-#(©)Codexbotz
+#Coded by @Its_Tartaglia_Childe
 
-from pyrogram import __version__
+from pyrogram import Client 
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -8,14 +8,28 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
+    if data == "help":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>",
+            text = f"<b>Bot Cammands\n❏ Commands For BOT Admins\n├/start : start the bot or get posts\n├/batch : create link for more than one posts\n├/genlink : create link for one post\n├/users : view bot statistics\n├/broadcast : broadcast any messages to bot users\n└/stats : checking your bot uptime\n\n👨‍💻 Developed by <a href=https://t.me/animeotaku109>AnimeOtaku™</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("💥​ᴄʟᴏꜱᴇ​💥", callback_data="close"),
+                        InlineKeyboardButton("⚡ᴀʙᴏᴜᴛ⚡", callback_data="about")
+                    ]
+                ]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text = f"<b>Owner - <a href=https://t.me/animeotaku109</a>\nMy Channel - <a href=https://t.me/animeotaku109>Anime Otaku</a>\nSupport Group - <a href=https://t.me/animeotakugc109>Otaku Support</a>\n\n👨‍💻 Developed by <a href=https://t.me/animeotaku109>Anime Oatku</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("💥​ᴄʟᴏꜱᴇ​💥", callback_data="close"),
+                        InlineKeyboardButton("🚀ʜᴇʟᴘ🚀", callback_data="help")
                     ]
                 ]
             )
